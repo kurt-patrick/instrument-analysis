ddc1091640e345dd27235b17d84c1952982651e8
kp <kurt_patrick_online@hotmail.com>
2018-08-05 19:17:36 +0800

    added top toolbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TopToolbarComponent } from './top-toolbar/top-toolbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopToolbarComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent,
+    TopToolbarComponent
+  ]
 })
 export class AppModule { }
---
+++ src/app/top-toolbar/top-toolbar.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'top-tool-bar',
+  templateUrl: './top-toolbar.component.html',
+  styleUrls: ['./top-toolbar.component.css']
+})
+export class TopToolbarComponent {
+  constructor() { }
+}
