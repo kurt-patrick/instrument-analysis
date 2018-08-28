b9f78b64d1172ae80e4a395f07ce605b915b6365
kp <kurt_patrick_online@hotmail.com>
2018-08-28 21:18:20 +0800

    implented bar data component showing 20 most recent bars

--- src/app/bar-data-paginated/bar-data-paginated.component.ts
+++ src/app/bar-data-paginated/bar-data-paginated.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { PriceBar } from './../price-bar';
+import { ChangeDetectionStrategy, Component, OnInit, Input } from '@angular/core';
+import { DataService } from './../shared/data.service';
 
 @Component({
   selector: 'app-bar-data-paginated',
   templateUrl: './bar-data-paginated.component.html',
-  styleUrls: ['./bar-data-paginated.component.css']
+  styleUrls: ['./bar-data-paginated.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class BarDataPaginatedComponent implements OnInit {
 
-  constructor() { }
+  page: 1;
+  instrumentCode: string;
+  priceBars: PriceBar[];
+
+  constructor(private data: DataService) {
+  }
+
+  barCount() {
+    if (!this.priceBars || this.priceBars.length === 0) {
+      return 0;
+    }
+    return this.priceBars.length;
+  }
 
   ngOnInit() {
+    this.data.instrumentCode.subscribe(newValue => {
+      this.instrumentCode = newValue;
+      this.priceBars = this.data.priceBars;
+    });
   }
 
 }
